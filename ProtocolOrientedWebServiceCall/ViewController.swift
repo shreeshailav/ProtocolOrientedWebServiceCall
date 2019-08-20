2e1a2583cfdd40fa9d51fc75af342d44edc8e885
Shreeshail <shreeshaila.v@hashedin.com>
2019-08-20 13:50:15 +0530

    Initial Commit

---
+++ ProtocolOrientedWebServiceCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProtocolOrientedWebServiceCall
+//
+//  Created by hasher on 20/08/19.
+//  Copyright © 2019 hasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
